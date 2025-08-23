e8f5d26d9876ae7f92b7f0e0f1c8dfd5443c7442
jaheen12 <topup7902@gmail.com>
2025-08-23 20:25:39 +0600

    feat: Configure professional Android CI/CD pipeline

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jaheen12.mathmateahc',
+  appName: 'MathMate AHC',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
