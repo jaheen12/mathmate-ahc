e4b1fcb893d461b42d1a4a17a35caf27d7252596
jaheen12 <topup7902@gmail.com>
2025-09-06 11:54:26 +0600

    feat: Updated app and main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { AuthProvider } from './AuthContext.jsx';
+import AuthProvider from './AuthContext.jsx';
 import { ToastContainer } from 'react-toastify';
 import './firebaseConfig.js'; // Initialize Firebase early
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // --- CHANGE: Import the service worker
